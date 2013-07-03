b664b7d5b5592c361ab46a16730cc1fa1e7e40b0
Arie <arie@wego.com>
2013-07-03 21:10:04 +0700

    add common variable string

---
+++ Tripify/library/netra/netraCommon.h
@@ -0,0 +1,18 @@
+//
+//  netraCommon.h
+//  Tripify
+//
+//  Created by Arie on 7/3/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface netraCommon : NSObject
+extern NSString * const api_key;
+extern NSString * const hotel_url;
+extern NSString * const flight_url;
+extern NSString * const deal_filter;
+extern NSString * const deal_url;
+
+@end
---
+++ Tripify/library/netra/netraCommon.m
@@ -0,0 +1,19 @@
+//
+//  netraCommon.m
+//  Tripify
+//
+//  Created by Arie on 7/3/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import "netraCommon.h"
+
+@implementation netraCommon
+NSString * const api_key=@"c055c1b373c6d9d84c0c";
+NSString * const hotel_url=@"http://api.wego.com/hotels/api/";
+NSString * const flight_url=@"http://www.wego.com/api/flights/";
+NSString * const deal_url=@"http://deals.wego.com/api/affiliates/deals.js?";
+NSString * const deal_filter=@"http://deals.wego.com/api/affiliates/filters.js?";
+
+
+@end
