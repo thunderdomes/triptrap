5ca0c164523a0d90ed3e866bfae64c1ba884e79e
Arie <arie@wego.com>
2013-07-04 00:06:23 +0700

    add new viewController

---
+++ Tripify/view/right/index/rightViewController.h
@@ -0,0 +1,13 @@
+//
+//  rightViewController.h
+//  Tripify
+//
+//  Created by Arie on 7/4/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rightViewController : UIViewController
+
+@end
