d2a0604084836ae5b381e616148befefae833415
Arie <arie@wego.com>
2013-07-03 20:41:33 +0700

    Initial Commit

---
+++ Tripify/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tripify
+//
+//  Created by Arie on 7/3/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "netraAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([netraAppDelegate class]));
+	}
+}
---
+++ Tripify/netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  Tripify
+//
+//  Created by Arie on 7/3/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
