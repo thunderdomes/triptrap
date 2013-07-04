bf6123d20fe09592cc50ca58c972e3e6d03e219e
Arie <arie@wego.com>
2013-07-04 23:39:30 +0700

    update instance ID method

--- Tripify/view/center/index/indexViewController.m
+++ Tripify/view/center/index/indexViewController.m
@@ -19,6 +19,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+		self.title=@"Flights";
+			[self fetchInstance];
     }
     return self;
 }
@@ -26,11 +28,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
 	// Do any additional setup after loading the view.
 }
+-(void)fetchInstance{
+	NSString *url=@"http://www.wego.com/api/flights/startSearch.html?format=json&fromLocation=SIN&toLocation=BKK&tripType=roundTrip&cabinClass=Economy&inboundDate=2013-08-26&outboundDate=2013-08-23&numAdults=1&apiKey=c055c1b373c6d9d84c0c";
+	
+	NSURL *URL=[NSURL URLWithString:url];
+	NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:URL cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60];
+	AFJSONRequestOperation *operation=[[AFJSONRequestOperation alloc] initWithRequest:request];
+	[AFJSONRequestOperation addAcceptableContentTypes:[NSSet setWithObject:@"application/json"]];
+	
+    //AFHTTPRequestOperation * operation =[[AFHTTPRequestOperation alloc] initWithRequest:request];
+	[operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+		NSLog(@"response Object--->%@",responseObject);
+		[self fetchDetails:[[responseObject objectForKey:@"request"]objectForKey:@"instanceId"]];
+	}failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		if(error){
+			
+			//[spinner stopAnimating];
+			//regional_radioList.hidden=YES;
+		}
+        NSLog(@"error: %@", [error description]);
+		
+	}];
+	[operation start];
+	
+	
 
+}
+-(void)fetchDetails:(NSString*)instanceId{
+	NSString *url=[NSString stringWithFormat:@"%@/pull.html?format=json&apiKey=%@&instanceId=%@&rand=1",flight_url,api_key,instanceId];
+	
+	NSURL *URL=[NSURL URLWithString:url];
+	NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:URL cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60];
+	AFJSONRequestOperation *operation=[[AFJSONRequestOperation alloc] initWithRequest:request];
+	//[AFJSONRequestOperation addAcceptableContentTypes:[NSSet setWithObject:@"application/json"]];
+	
+    //AFHTTPRequestOperation * operation =[[AFHTTPRequestOperation alloc] initWithRequest:request];
+	[operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+		NSLog(@"response Object--->%@",responseObject);
+	}failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		if(error){
+			//[spinner stopAnimating];
+			//regional_radioList.hidden=YES;
+		}
+        NSLog(@"error: %@", [error description]);
+		
+	}];
+	[operation start];
+}
 - (void)didReceiveMemoryWarning
 {
+	
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
